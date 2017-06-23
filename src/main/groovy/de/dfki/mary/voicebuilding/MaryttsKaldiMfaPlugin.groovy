501d881b925364588c96d4ba0955e3ceae2d0343
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-06-23 15:58:23 +0200

    add PrepareForcedAlignment task class

--- src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
@@ -1,5 +1,6 @@
 package de.dfki.mary.voicebuilding
 
+import de.dfki.mary.voicebuilding.tasks.PrepareForcedAlignment
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,6 +8,6 @@ class MaryttsKaldiMfaPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-
+        project.task('prepareForcedAlignment', type: PrepareForcedAlignment)
     }
 }
---
+++ src/main/groovy/de/dfki/mary/voicebuilding/tasks/PrepareForcedAlignment.groovy
@@ -0,0 +1,53 @@
+package de.dfki.mary.voicebuilding.tasks
+
+import org.gradle.api.*
+import org.gradle.api.tasks.*
+
+class PrepareForcedAlignment extends DefaultTask {
+
+    @InputDirectory
+    File maryXmlDir
+
+    @InputDirectory
+    File wavDir
+
+    @OutputDirectory
+    File forcedAlignmentDir
+
+    @TaskAction
+    void prepare() {
+        def dict = [:]
+        def broken = []
+        project.fileTree(maryXmlDir).include('*.xml').each { xmlFile ->
+            try {
+                def tokens = []
+                new XmlSlurper().parse(xmlFile).'**'.findAll { it.name() == 't' }.each { token ->
+                    def phonemes = token.'**'.findAll { it.name() == 'syllable' }.collect { syllable ->
+                        syllable.ph.collect { it.@p }
+                    }.flatten().join(' ')
+                    if (phonemes) {
+                        dict[token.text()] = phonemes
+                        tokens << token.text()
+                    }
+                }
+                project.file("$forcedAlignmentDir/${xmlFile.name - '.xml' + '.lab'}").withWriter('UTF-8') { out ->
+                    out.println tokens.join(' ')
+                }
+            } catch (all) {
+                project.logger.error "Excluding $xmlFile.name"
+                broken << xmlFile.name - '.xml'
+            }
+        }
+        project.file("$forcedAlignmentDir/dict.txt").withWriter('UTF-8') { out ->
+            dict.toSorted { it.key.toLowerCase() }.each { word, phonemes ->
+                out.println "$word $phonemes"
+            }
+        }
+        project.copy {
+            from wavDir
+            into forcedAlignmentDir
+            include '*.wav'
+            exclude broken.collect { it + '.wav' }
+        }
+    }
+}
