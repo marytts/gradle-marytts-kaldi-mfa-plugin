dd01f3625fa2c223886f18d58be6da5ccc42bb9b
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-06-23 13:01:21 +0000

    init barebones plugin

---
+++ src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
@@ -0,0 +1,12 @@
+package de.dfki.mary.voicebuilding
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MaryttsKaldiMfaPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
