5513fbe0840c64da14dbb6218873359ad39e340a
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-09-20 14:50:32 +0200

    download and unpack native MFA release
    TODO
    - add logic to switch which archive by OS

--- src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
@@ -1,13 +1,16 @@
 package de.dfki.mary.voicebuilding
 
 import de.dfki.mary.voicebuilding.tasks.*
+import de.undercouch.gradle.tasks.download.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 class MaryttsKaldiMfaPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        project.pluginManager.apply DownloadTaskPlugin
         project.configurations {
             marytts
         }
@@ -23,8 +26,18 @@ class MaryttsKaldiMfaPlugin implements Plugin<Project> {
             dependsOn project.convertTextToMaryXml
             maryXmlDir = project.convertTextToMaryXml.destDir
         }
+        project.task('downloadMFA', type: Download) {
+            src 'https://github.com/MontrealCorpusTools/Montreal-Forced-Aligner/releases/download/v1.0.0/montreal-forced-aligner_macosx.zip'
+            dest project.buildDir
+            overwrite false
+        }
+        project.task('unpackMFA', type: Copy) {
+            dependsOn project.downloadMFA
+            from project.zipTree("$project.buildDir/montreal-forced-aligner_macosx.zip")
+            into "$project.buildDir/mfa"
+        }
         project.task('runForcedAlignment', type: RunForcedAlignment) {
-            dependsOn project.prepareForcedAlignment
+            dependsOn project.prepareForcedAlignment, project.unpackMFA
             srcDir = project.prepareForcedAlignment.forcedAlignmentDir
         }
         project.task('convertTextGridToXLab', type: ConvertTextGridToXLab) {
