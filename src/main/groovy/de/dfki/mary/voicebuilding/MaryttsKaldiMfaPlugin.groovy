1eb25c22c734a85f9a9eda2a238befd696cc1088
Ingmar Steiner <steiner@coli.uni-saarland.de>
2021-04-21 15:20:58 +0200

    Fix dependency resolution

--- src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
+++ src/main/groovy/de/dfki/mary/voicebuilding/MaryttsKaldiMfaPlugin.groovy
@@ -24,6 +24,9 @@ class MaryttsKaldiMfaPlugin implements Plugin<Project> {
                 patternLayout {
                     artifact '[revision]-[classifier].[ext]'
                 }
+                metadataSources {
+                    artifact()
+                }
             }
         }
 
